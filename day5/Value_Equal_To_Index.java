ccf28a604cad5e5d6627facbb98c65d907001f5d
sameer-tiwari <sameertiwari0112@gmail.com>
2022-10-15 00:22:39 +0530

    commiting files

---
+++ day5/Value_Equal_To_Index.java
@@ -0,0 +1,16 @@
+
+
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            
+            if(arr[i]==(i+1)){ list.add(i+1); }
+        
+    }
+return list;
+        
+        
+    }
+}
